c1e98c414aa6e4edae1de50634200d57686e9333
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-11-23 14:22:19 -0300

    comitar gravarDatas() ainda no login

--- Spring/src/main/java/Spring/Spring/DAO/InterfaceTelefone.java
+++ Spring/src/main/java/Spring/Spring/DAO/InterfaceTelefone.java
@@ -2,6 +2,9 @@ package Spring.Spring.DAO;
 
 import java.util.List;
 
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +15,7 @@ import Spring.Spring.Model.Usuario;
 @Repository
 public interface InterfaceTelefone extends CrudRepository<Telefone, Long> {
 	
+	@Lock(LockModeType.READ)
 	@Query(value = "SELECT p FROM Telefone p WHERE p.usuario = ?1")
 	public List<Telefone> buscarPorId(Usuario usuario);
 }
--- Spring/src/main/java/Spring/Spring/DAO/InterfaceUsuario.java
+++ Spring/src/main/java/Spring/Spring/DAO/InterfaceUsuario.java
@@ -2,8 +2,10 @@ package Spring.Spring.DAO;
 
 import java.util.List;
 
+import javax.persistence.LockModeType;
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -15,9 +17,11 @@ import Spring.Spring.Model.Usuario;
 @Repository
 public interface InterfaceUsuario extends CrudRepository<Usuario, Long> {
 
+	@Lock(LockModeType.READ)
 	@Query(value = "SELECT p FROM Usuario p WHERE p.nome LIKE %?1%")
 	public List<Usuario> buscarPorNome(String nome);
 	
+	@Lock(LockModeType.READ)
 	@Query(value = "SELECT p.nome FROM Usuario p WHERE p.nome = :paramnome")
 	public String buscarUsuarioPorNome(@Param("paramnome") String nome);
 	
